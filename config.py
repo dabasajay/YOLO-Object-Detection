b24b63f2bcaeb381922ce1862ad7bcc950886aea
Ajay Dabas <se.dabasajay@gmail.com>
2019-06-03 20:05:53 +0530

    added configuration file

---
+++ config.py
@@ -0,0 +1,13 @@
+config = {
+    'keras_model_path' : 'model_data/yolov2.hdf5',
+    'model_weights': 'model_data/yolov2.weights',
+    'model_cfg': 'model_data/yolov2.cfg',
+    'anchors_path' : 'model_data/yolo_anchors.txt',
+    'classes_path' : 'model_data/coco_classes.txt',
+    'test_path' : 'images',
+    'output_path' : 'output',
+    'score_threshold' : 0.4,
+    'iou_threshold' : 0.4,
+    'random_seed': 10101,
+    'font_path': 'font/FiraMono-Medium.otf'
+}
